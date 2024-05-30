943077e39cbc272b1b177c0b15824a76f997baef
neil <stream.neil@gmail.com>
2024-05-30 08:22:29 +0800

    print excepiton str(e)

--- job_scrapy_project/utils/utils.py
+++ job_scrapy_project/utils/utils.py
@@ -10,7 +10,7 @@ def get_request(url, headers, params=None):
         assert response.status_code == 200
         return json.loads(response.content.decode('utf-8'))
     except Exception as e:
-        log('Request Error get_request, e:', e)
+        log('Request Error get_request, e:', str(e))
         return None
 
 def post_request(url, headers=None, data=None, json_data=None):
@@ -19,7 +19,7 @@ def post_request(url, headers=None, data=None, json_data=None):
         assert response.status_code == 200
         return json.loads(response.content.decode('utf-8'))
     except Exception as e:
-        log('Request Error post_request, e:', e)
+        log('Request Error post_request, e:', str(e))
         return None
 
 def create_cookie_header(cookie_dict):
